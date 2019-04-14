53f300d2e6536df4ef561d5c57c0d0fc43d02f03
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-04-14 03:07:34 +0000

    vince: prop: Add back use_fifo_ui
    check https://github.com/LineageOS/android_device_xiaomi_msm8953-common/commit/c7e982a0fc0a264b0753a135a9597b78e2e9f760

--- prop.mk
+++ prop.mk
@@ -227,6 +227,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 sys.use_fifo_ui=1
 
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+sys.use_fifo_ui=1
+
 # Usb
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.usb.config.extra=none
