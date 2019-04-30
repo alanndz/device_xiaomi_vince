80e9af949030e3f907e8cfd2f53a8cf5d3013dd2
alanndz <alanmahmud0@gmail.com>
2019-04-30 12:14:43 +0800

    Remove Gcam

--- device.mk
+++ device.mk
@@ -144,7 +144,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    Gcam
 
 # Consumerir
 PRODUCT_PACKAGES += \
